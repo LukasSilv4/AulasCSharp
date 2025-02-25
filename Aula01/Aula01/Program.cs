4a9c172606503dd6ad9b1f8cb2b914bab5588f38
LukasSilv4 <Lukascavalcante78@gmail.com>
2025-02-25 19:53:03 -0300

    Primeira alteração

--- Aula01/Aula01/Program.cs
+++ Aula01/Aula01/Program.cs
@@ -9,7 +9,7 @@ string surname = " Cavalcante ";
 
 Console.WriteLine("Meu nome completo é " + firstname + " "+ surname + " " +lastname);
 
-int idade = 24;
+//int idade = 24;
 
 Console.WriteLine($"Meu nome completo é  {firstname} {surname} {lastname} e a minha idade é {idade}");
 
@@ -17,7 +17,7 @@ Console.WriteLine("Escreva seu nome: ");
 
 string nome = Console.ReadLine();
 
-Console.WriteLine($"Olá {nome}, boa noite!!!");
+Console.WriteLine($"Olá {nome}, boa noite ! ! ! ");
 
 
 
