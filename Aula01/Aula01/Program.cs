e1ed9e67e5d4354d6b87b3aecc4395043c7ed6b5
lucas.csilva122 <lucas.csilva122@TIT0533383W10-1>
2025-02-25 19:27:14 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Lucas");
+Console.WriteLine("Silva");
+
+string firstname = " Lucas ";
+string lastname = " Da Silva ";
+string surname = " Cavalcante ";
+
+Console.WriteLine("Meu nome completo é " + firstname + " "+ surname + " " +lastname);
+
+int idade = 24;
+
+Console.WriteLine($"Meu nome completo é  {firstname} {surname} {lastname} e a minha idade é {idade}");
+
+Console.WriteLine("Escreva seu nome: ");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Olá {nome}, boa noite!!!");
+
+
+
+
