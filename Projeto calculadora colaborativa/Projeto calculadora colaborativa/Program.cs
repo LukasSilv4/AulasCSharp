7f4ff3a341280e625fd1aa0adae9c26aba88abd3
LukasSilv4 <Lukascavalcante78@gmail.com>
2025-02-26 22:06:02 -0300

    soma e subtração final

---
+++ Projeto calculadora colaborativa/Projeto calculadora colaborativa/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        public static int Somar(int num1, int num2);
+
+
+        int num1;
+        int num2;
+        int soma;
+        int subtracao;
+
+        Console.WriteLine("Digite o primeiro Número: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o Segundo número: ");
+        num2 = int.Parse(Console.ReadLine());
+        soma = num1 + num2;
+        Console.WriteLine("A Soma Dos dois Números é {0}", soma);
+    }
+}
---
+++ teste/teste/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Calculadora
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.WriteLine("\nQual operação você deseja realizar?");
+        Console.WriteLine("Digite 3 para Soma");
+        Console.WriteLine("Digite 4 para Subtração");
+
+
+        int escolha = Convert.ToInt32(Console.ReadLine());
+
+
+        switch (escolha)
+        {
+            case 3:
+                Console.WriteLine("Soma: " + (num1 + num2));
+                break;
+
+            case 4:
+                Console.WriteLine("Subtração: " + (num1 - num2));
+                break;
+
+            default:
+                Console.WriteLine("Escolha inválida! Por favor, insira um número entre 1 e 4.");
+                break;
+        }
+    }
+
+
+}
