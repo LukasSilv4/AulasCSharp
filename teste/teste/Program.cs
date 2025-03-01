49f1a9b4b77423d135d7baa31d8f566e0d244818
LukasSilv4 <Lukascavalcante78@gmail.com>
2025-02-28 22:05:50 -0300

    Projeto Calculadora windows form

--- teste/teste/Program.cs
+++ teste/teste/Program.cs
@@ -7,20 +7,21 @@ class Calculadora
         Console.Write("Digite o primeiro número: ");
         double num1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Digite o segundo número: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
-
-
-        Console.WriteLine("\nQual operação você deseja realizar?");
-        Console.WriteLine("Digite 3 para Soma");
-        Console.WriteLine("Digite 4 para Subtração");
+        {
+            Console.WriteLine("\nQual operação você deseja realizar?");
+            Console.WriteLine("Digite 3 para Soma");
+            Console.WriteLine("Digite 4 para Subtração");
+        }
+        
+            double num2 = Convert.ToDouble(Console.ReadLine());
+        
 
 
         int escolha = Convert.ToInt32(Console.ReadLine());
-
-
         switch (escolha)
         {
+
+
             case 3:
                 Console.WriteLine("Soma: " + (num1 + num2));
                 break;
@@ -32,7 +33,18 @@ class Calculadora
             default:
                 Console.WriteLine("Escolha inválida! Por favor, insira um número entre 1 e 4.");
                 break;
+         
         }
+        Console.Write("Digite o segundo número: ");
+
+
+
+    
+
+       
+
+
+      
     }
 
 
